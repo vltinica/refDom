35a46f5bbd8540ebc736700b380c2bb94d245f39
Vladimir Tinica <vltinica@gmail.com>
2022-02-13 21:14:16 -0500

    start

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import React from "react";
+import "./goods.json";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hey</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
